804250b1b4a61897621eecd35d713c55c712d511
Pranay Kotian <lohitkotian@yahoo.ca>
2019-11-03 12:49:37 -0500

    Add files via upload

---
+++ ToLowerCase.py
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/to-lower-case/
+//Title: To Lower Case
+//Difficulty: Easy
+//Language: Python3
+//Author: Pranay Kotian
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
