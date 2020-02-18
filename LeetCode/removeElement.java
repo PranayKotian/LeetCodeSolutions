cff6b0b925e8634772cf81e1ff3f9e5aef63890d
PranayKotian <lohitkotian@yahoo.ca>
2020-02-18 09:49:13 -0500

    remove element java solution added

---
+++ LeetCode/removeElement.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/remove-element/
+//Title: Remove Element
+//Difficulty: Easy
+//Language: Java
+//Author: Pranay Kotian
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        /*
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                i -= 1;
+                for (int j = i + 1; j < nums.length; j++) {
+                    nums[j - 1] = nums[j];
+                }
+            }
+            else
+                length += 1;
+        }
+        return length + 1; 
+
+        //Solution exceeds time limit when i is decremented by 1
+        */
+        
+    }
+}
