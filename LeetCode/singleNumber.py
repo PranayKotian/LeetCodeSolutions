f6e64602b9f2eb7cfc4f222db15c66a3ce084c69
PranayKotian <lohitkotian@yahoo.ca>
2020-02-29 14:52:31 -0500

    single number problem

---
+++ LeetCode/singleNumber.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/single-number/
+#Title: Single Number
+#Difficulty: Easy
+#Language: Python3
+#Author: Pranay Kotian
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if (i%2 == 0) and (nums[i] != nums[i+1]):
+                return nums[i]
+        return nums[len(nums) - 1]
