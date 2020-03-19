34ed3f24053ba2d6f8c25505e05a2eb7648872a6
PranayKotian <lohitkotian@yahoo.ca>
2020-03-19 00:03:47 -0400

    completed paraentheses solution with comments

--- LeetCode/isValid.py
+++ LeetCode/isValid.py
@@ -6,16 +6,29 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        
         stack = []
+        
+        #If closed paraentheses matches open parantheses at the top of the stack
+        # delete that parantheses from the stack
+        # else, add paraentheses to stack 
+        # Valid parantheses if stack is empty in the end
         for i in s:
-            if (i == ")"):
-                if stack[-1] == "(":
-                    del stack[-1]
-                
-            if (i == "}"):
-                if stack[-1] == "(":
-                    del stack[-1]
-                
-            if (i == "]"):
-                if stack[-1] == "(":
-                    del stack[-1]
+            
+            #Checks if stack is empty
+            if (i == ")" or i == "}" or i == "]") and len(stack) == 0:
+                return False
+            
+            if (i == ")") and (stack[-1] == "("):
+                del stack[-1]
+            elif (i == "}") and (stack[-1] == "{"):
+                del stack[-1]
+            elif (i == "]") and (stack[-1] == "["):
+                del stack[-1]
+            else:
+                stack.append(i)
+        
+        if len(stack) == 0:
+            return True
+        else:
+            return False
