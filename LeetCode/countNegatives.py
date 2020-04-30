3bf86df49f3db4c62db27bc7f99b9d81b192cba0
Pranay Kotian <lohitkotian@yahoo.ca>
2020-04-29 23:43:00 -0400

    countNegative solution

---
+++ LeetCode/countNegatives.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        """
+        count = 0
+        
+        for i in grid:
+            for j in range(len(i) - 1, -1, -1):
+                if i[j] < 0:
+                    count += 1
+                    
+        return count
+        """ 
+        
+        count = 0
+        rows = len(grid) 
+        colPos = len(grid[0]) -1 
+        
+        for i in grid:
+            #iterates through rows
+            for j in range(colPos, -1, -1):
+                if i[j] < 0:
+                    count += rows
+                    #if neg val is found entire col is elimated cus all vals would be ne
+                    colPos -= 1
+                else: 
+                    #once positive val is hit we know there are no more negative numbers in the row
+                    continue
+            rows -= 1
+        
+        return count
+        
