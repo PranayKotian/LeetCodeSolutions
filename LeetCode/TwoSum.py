263b13ee6aba147f00101627efe6c79b89afc70b
kotianp <kotianp@mcmaster.ca>
2021-06-05 03:36:05 -0400

    better two sum solution

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -6,10 +6,21 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length = len(nums)
-        for i in range(length):
-            j = i + 1
-            while j < length:
-                if target == (nums[i] + nums[j]):
-                    return [i, j];
-                j += 1
+        #BRUTE FORCE SOLUTION (very slow 6400ms)
+        # length = len(nums)
+        # for i in range(length):
+        #     j = i + 1
+        #     while j < length:
+        #         if target == (nums[i] + nums[j]):
+        #             return [i, j];
+        #         j += 1
+        
+        #HASH TABLE SOLUTION (find better code for duplicate num/compliment case)
+        n = len(nums)
+        for i in range(n):
+            comp = target - nums[i]
+            if (comp == nums[i]):
+                if (comp in nums[i+1:n]):
+                    return [i, nums[i+1:n].index(comp) + i + 1]
+            elif (comp in nums):
+                return [i, nums.index(comp)]
