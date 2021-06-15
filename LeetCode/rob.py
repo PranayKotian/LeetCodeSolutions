27e3a8adae5e18f90852d7972ad0db9de14e6fe1
kotianp <kotianp@mcmaster.ca>
2021-06-15 18:37:29 -0400

    house robber solution

---
+++ LeetCode/rob.py
@@ -0,0 +1,25 @@
+#https://leetcode.com/problems/house-robber/
+#Title: House Robber
+#Difficulty: Medium
+#Language: Python3
+#Author: Pranay Kotian
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return nums[0]
+        
+        arr = [0 for i in range(n)]
+        arr[0] = nums[0]
+        arr[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            arr[i] = max(arr[i-1], arr[i-2] + nums[i])
+        
+        return arr[n-1]
+    
+        """
+        Runtime: 32 ms, faster than 65.46% of Python3 online submissions for House Robber.
+        Memory Usage: 14.5 MB, less than 17.94% of Python3 online submissions for House Robber.
+        """
