2b6e96bf079d18ab98579d6698374db8f55767ae
PranayKotian <lohitkotian@yahoo.ca>
2022-01-28 23:59:49 -0500

    deleted 3sum

--- LeetCode/threeSum.py
+++ LeetCode/threeSum.py
@@ -6,105 +6,43 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        '''
-		PSEUDOCODE
-		1. Sort array
-		2. Create empty arr for answers and initialize length of arr into variable to save time on repeated len calls
-		3. Iterate through list of nums using for loop
-		4. Set condition to skip repeated targets (if cur elm = last elm)
-		5. Set target
-		6. Initialize start and end variables
-		7. While loop through elms in front of target to find triplets
-		8. If triplet is found iterate start to avoid repeat triplets being added to arr
-		9. Else, if start + end > target decrement end to get closer to target
-		10. Else if start + end < target increment start to get closer to target
-		11. Stop while loop once start > end
-		12. Return arr with triplets for 3Sum
-        '''
-
-        #Sort array
-        nums.sort()
-        arr = []
-        N = len(nums)
-        
-        for i in range(N):
-            #Skips over checking for the same target twice
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
-            #Looks for two numbers that equals target
-            target = nums[i] * -1
-            
-            #Checks numbers from opposite ends that equal the target
-            start = i + 1
-            end = N - 1
-            while start < end:
-                if (nums[start] + nums[end] == target):
-                    arr.append([nums[i], nums[start], nums[end]])
-                    
-                    #avoids duplicate triplets 
-                    start = start + 1
-                    while start < end and (nums[start] == nums[start-1]):
-                        start = start + 1
-                    
-                elif (nums[start] + nums[end] > target):
-                    end = end - 1
-                else:
-                    start = start + 1
                 
-        return arr
+        nums.sort()
+        n = len(nums)
         
-
+        if (n < 3):
+            return []
+        triplets = []
         
-        """
-        MY OLD SOLUTION:
-        arr = []
-        
-        i = 0
-        N = len(nums)
-        while (i < N):
-            j = 1 + 1
-            while (j < N):
-                k = j + 1
-                while (k < N):
-                    if (nums[i] + nums[j] + nums[k] == 0):
-                        arr.append([nums[i], nums[j], nums[k]])
-            i += 1
-        
-        return arr
-
-        #EXCEEDS TIME LIMIT
-        """
-        
-        
-        """
-        #nums = complete list of nums
-        
-        1 2 3 4
-          2 3 4 5 
-            3 4 5 6
+        for i in range(n-2):
+            if (i != 0 and nums[i] == nums[i-1]):
+                continue
+#            target = -nums[i]
             
-        
-        listList = []
-        
-        nums1 = nums.copy()
-        nums1.pop(-1)
-        nums1.pop(-2)
-        nums2 = nums.copy()
-        nums2.pop(0)
-        nums2.pop(-1)
-        nums3 = nums.copy()
-        nums3.pop(0)
-        nums3.pop(1)
-        
-        for i in nums1:
-            for j in nums2:
-                for k in nums3:
-                    if (i + j + k == 0):
-                        listList.append([i, j, k])
-        return listList
-        
-        """
-        
+            l = i + 1
+            r = n - 1
+            
+            #EFFICIENT L R pointer solution (requires sorting nums array)
+            while r > l:
+                if (nums[i] + nums[l] + nums[r] > 0):
+                    r -= 1
+                elif (nums[i] + nums[l] + nums[r] < 0):
+                    l += 1
+                else: #(nums[i] + nums[l] + nums[r] == 0)
+                    triplets.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while (nums[l] == nums[l-1] and r > l):
+                        l += 1
                         
-        
+            #2SUM code and target determine by looping through list
+            # worst case scenario is correct target is third last elemenet with next two elements working
+#             for j in range(i + 1, n-1):
+#                 comp = target - nums[j]
+#                 if (j != 1 and comp == oldcomp):
+#                     continue
+                
+#                 if (comp in nums[j+1:n]):
+#                     triplets.append([nums[i], nums[j], comp])
+#                 oldcomp = comp    
+            
+        return triplets
