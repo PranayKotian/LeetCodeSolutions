d16c63989450909e69d8622be2feec49caf91589
PranayKotian <lohitkotian@yahoo.ca>
2022-03-02 14:41:22 -0500

    longest common subsequence solution

--- LeetCode/Python/longestCommonSubsequence.py
+++ LeetCode/Python/longestCommonSubsequence.py
@@ -1,29 +1,19 @@
 #https://leetcode.com/problems/longest-common-subsequence/
-#Title: Longest Common Subsequence
+#Title: 1143. Longest Common Subsequence
 #Difficulty: Medium
-#Language: Python3
+#Language: Python
 #Author: Pranay Kotian
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        #intiialize empty 2D array of width and height of the text lengths
-        rows, cols = (len(text1) + 1, len(text2) + 1)
-        arr = [[0 for a in range(cols)] for b in range(rows)]
+        arr = [[0 for i in range(len(text1)+1)] for j in range(len(text2)+1)]
         
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if (text1[i-1] == text2[j-1]):
-                    arr[i][j] = 1 + arr[i-1][j-1]
+        for j in range(1,len(text2)+1):
+            for i in range(1,len(text1)+1):
+                if text2[j-1] == text1[i-1]:
+                    arr[j][i] = arr[j-1][i-1] + 1
                 else:
-                    if arr[i-1][j] > arr[i][j-1]:
-                        arr[i][j] =  arr[i-1][j]
-                    else:
-                        arr[i][j] =  arr[i][j-1]
+                    arr[j][i] = max(arr[j-1][i], arr[j][i-1])
         
-        return arr[rows-1][cols-1]
-
-        '''
-        Runtime: 376 ms, faster than 90.77% of Python3 online submissions for Longest Common Subsequence.
-        Memory Usage: 22.8 MB, less than 40.69% of Python3 online submissions for Longest Common Subsequence.
-        '''
+        return arr[len(text2)][len(text1)]
