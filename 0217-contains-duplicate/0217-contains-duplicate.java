d63b5d65368c16522afde1e2643c91983ef78dd7
Pranay Kotian <lohitkotian@yahoo.ca>
2023-11-29 02:36:10 -0500

    Time: 10 ms (83.58%), Space: 55.4 MB (53.90%) - LeetHub
    Attach NOTES - LeetHub
    Create README - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        //Hashtable<Integer, Integer> ht1 = new Hashtable<>();
+//         Hashtable ht1 = new Hashtable();
+        
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (ht1.contains(nums[i])) {
+        //         return false;
+        //     }
+        //     ht1.put(i, nums[i]);
+        // }
+        
+//         //If no duplicates are found
+//         return true; 
+        
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        
+        return false; 
+    }
+}
