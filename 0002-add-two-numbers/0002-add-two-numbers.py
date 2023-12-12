9b54d7cc48b2642484b981ed3bddc8b9f54bed3d
Pranay Kotian <lohitkotian@yahoo.ca>
2023-12-12 02:26:40 -0500

    Time: 65 ms (57.38%), Space: 16.4 MB (47.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,27 +6,18 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        #Solution 1: conversions
-        #pass through both lists
-        #convert to int
-        #add the ints
-        #convert to linked list
-        
-        s1 = str(l1.val)
-        while l1.next:
-            l1 = l1.next
-            s1 = str(l1.val) + s1
-        s2 = str(l2.val)
-        while l2.next:
-            l2 = l2.next
-            s2 = str(l2.val) + s2
-        total = int(s1) + int(s2)
-        
-        res = ListNode()
-        p1 = res
-        for c in str(total)[::-1]:
-            p1.next = ListNode(int(c))
-            p1 = p1.next
+        #Solution 2: generate answer while passing through l1, l2
+        carry = 0
+        head = res = ListNode()
+        while l1 or l2 or carry:
+            cur = carry
+            if l1:
+                cur += l1.val
+                l1 = l1.next
+            if l2:
+                cur += l2.val
+                l2 = l2.next
+            carry, cur = divmod(cur, 10)
+            head.next = ListNode(cur)
+            head = head.next
         return res.next
-        
-        #Solution 2: ??
