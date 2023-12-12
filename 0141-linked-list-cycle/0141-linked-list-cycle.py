b64879fc6228d6504ce6c8dc772df16e20ffc057
Pranay Kotian <lohitkotian@yahoo.ca>
2023-12-12 02:33:20 -0500

    Time: 66 ms (14.77%), Space: 20.6 MB (21.98%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #Solution 1: track all visited nodes O(n) time, O(n) space solution
+        
+        #Solution 2: fast and slow pointers O(n) time, O(1) space
+        if head is None or head.next is None:
+            return False
+        
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
