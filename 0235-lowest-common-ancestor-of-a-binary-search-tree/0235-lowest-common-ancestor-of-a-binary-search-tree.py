fb2a2c3aae219cfde8dae64fd3088e146d4e95b8
Pranay Kotian <lohitkotian@yahoo.ca>
2023-12-14 15:41:08 -0500

    Time: 53 ms (99.75%), Space: 20.8 MB (44.90%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,19 +7,10 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #Solution 1: check all nodes going down
-            #top node is guaranteed to be an ancestor
-            #check to see if p and q are connected to current node
-            # checking downwards
-            #if p or q == current node: return that node
-            
         #Solution 2
-            #dfs for both nodes
-            #take the pointers and move upwards
-            #while keeping pointers on the same level
-            #when p1 = q1: return that node
-            
-            #track current level of node
+            #search for both nodes
+            #track parents of both nodes
+            #find the lowest parent
         
         def search(cur: 'TreeNode', parents: 'list', node: 'TreeNode'):
             if cur is None:
@@ -33,9 +24,7 @@ class Solution:
         
         p1 = search(root, [], p)
         q1 = search(root, [], q)
-        
-        res = p1[0]
-        for i in range(min(len(p1),len(q1))):
-            if p1[i].val == q1[i].val:
-                res = p1[i]
-        return res
+        i = 0
+        while i < min(len(p1),len(q1)) and p1[i].val == q1[i].val:
+            i += 1
+        return p1[i-1]
