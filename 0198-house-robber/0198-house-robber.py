34385d3cc5c147a8d4666cbf0d40ff2dcb3b6b84
Pranay Kotian <lohitkotian@yahoo.ca>
2023-12-21 19:52:36 -0500

    Time: 42 ms (39.64%), Space: 17.2 MB (7.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        nums = [0] + nums
-        maxProfit = nums.copy()
-        for i in range(2, len(maxProfit)):
-            maxProfit[i] = max(maxProfit[i-2] + nums[i], maxProfit[i-1])
-        
-        return maxProfit[-1]
+        p1 = 0
+        p2 = nums[0]
+        for i in range(1, len(nums)):
+            p2, p1 = max(p1 + nums[i], p2), p2        
+        return p2
