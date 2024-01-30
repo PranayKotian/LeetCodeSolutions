5b06a6430acae12d9ffadbb3392f8198d842e0e1
Pranay Kotian <lohitkotian@yahoo.ca>
2024-01-30 17:21:40 -0500

    Time: 100 ms (92.90%), Space: 17.5 MB (71.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
         #Greedy solution
         #Time: O(n) Space: O(1)
         jumps = 0
-        cur = 0
-        r = min(nums[0], len(nums)-1)
+        l = r = 0
         
-        while cur != len(nums)-1:
+        while r < len(nums)-1:
             jumps += 1
-            temp = r
-            for i in range(cur+1,r+1):
-                r = max(r, i+nums[i])
-            r = min(r, len(nums)-1)
-            cur = temp
+            newr = 0
+            for i in range(l,r+1):
+                newr = max(newr, i+nums[i])
+            l = r+1
+            r = newr
         
         return jumps
