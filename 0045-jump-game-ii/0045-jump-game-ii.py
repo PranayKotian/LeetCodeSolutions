777105b212964ed227497e4aa78d5b99fb27222a
Pranay Kotian <lohitkotian@yahoo.ca>
2024-01-30 17:17:59 -0500

    Time: 99 ms (94.45%), Space: 17.5 MB (71.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,11 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        minJumps = [10**5 for i in range(len(nums))]
-        minJumps[0] = 0
         
-        for n in range(len(nums)):    
-            for i in range(n+1,min(n+1+nums[n],len(nums))):
-                minJumps[i] = min(minJumps[i], minJumps[n]+1)
+        #Greedy solution
+        #Time: O(n) Space: O(1)
+        jumps = 0
+        cur = 0
+        r = min(nums[0], len(nums)-1)
         
-        return minJumps[-1]
+        while cur != len(nums)-1:
+            jumps += 1
+            temp = r
+            for i in range(cur+1,r+1):
+                r = max(r, i+nums[i])
+            r = min(r, len(nums)-1)
+            cur = temp
         
+        return jumps
