7dba69283762d318124fd309cbd4b87bb92de423
Pranay Kotian <lohitkotian@yahoo.ca>
2024-01-30 21:39:54 -0500

    Time: 524 ms (82.83%), Space: 31.2 MB (57.38%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #Greedy solution
+        #Time: O(n) Space: O(1)
+        
+        
+        res = nums[0]
+        cur = 0
+        l = 0
+        for r in range(len(nums)):
+            cur += nums[r]
+            res = max(res,cur)
+            while cur < 0 and l <= r:
+                cur -= nums[l]
+                l += 1
+        
+        return res
+                
+        
