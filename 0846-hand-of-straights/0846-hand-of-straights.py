a4d7bd232b01a0d61a0aceea14b9149b60b973c3
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-01 09:50:46 -0500

    Time: 508 ms (12.06%), Space: 18.5 MB (68.86%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,5 +1,28 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        #Efficient solution?
+        #Time: O(n) Space: O(n)
+        
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = {}
+        for n in hand:
+            count[n] = count.get(n,0) + 1
+        
+        while count:
+            minVal = min(count.keys())
+            for i in range(groupSize):
+                if minVal not in count:
+                    return False
+                count[minVal] -= 1
+                if count[minVal] == 0:
+                    del count[minVal]
+                minVal += 1
+        
+        return True
+        
+        """
         #Inefficient solution
         #Time: O(n^2) Space: O(n)
         if len(hand) % groupSize != 0:
@@ -16,3 +39,4 @@ class Solution:
                     return False
                 hand.remove(minVal) #O(n) operation
         return True
+        """
