fab60e6f208cfb40c32aa3669aedbf8ab83048d3
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-06 11:47:48 -0500

    Time: 1045 ms (5.03%), Space: 472.3 MB (5.19%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,20 +1,24 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        #Solution 2: Recursive w/ lazy caching
+        #Solution 2: Recursive w/ caching
         #Time: Space:
         
-        @lru_cache(None)
+        cache = {}
         def backtrack(s1,t1):
             if t1 == "":
                 return 1
             if s1 == "":
                 return 0
+            if (s1,t1) in cache:
+                return cache[(s1,t1)]
             
             if s1[0] != t1[0]:
-                return backtrack(s1[1:],t1)
+                res = backtrack(s1[1:],t1)
             else:
-                return backtrack(s1[1:],t1[1:]) + backtrack(s1[1:],t1)
+                res = backtrack(s1[1:],t1[1:]) + backtrack(s1[1:],t1)
+            cache[(s1,t1)] = res
+            return res
         
         return backtrack(s,t)
         
