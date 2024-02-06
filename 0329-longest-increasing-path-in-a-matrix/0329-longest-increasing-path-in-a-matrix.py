450e6f83f2b4bc5adb566466ab6339525e834c1c
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-06 11:32:32 -0500

    Time: 297 ms (78.12%), Space: 21 MB (42.49%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        
+        #Solution 1: Dynamic Programming
+        #Time: O(n*m) Space: O(n*m)
+        
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        
+        dp = [[1 for _ in range(COLS)] for _ in range(ROWS)]
+        cache = {}
+        
+        def dfs(r,c,prev):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or matrix[r][c] <= prev:
+                return 0
+            if (r,c) not in cache:
+                cur=matrix[r][c]
+                cache[(r,c)] = 1 + max(dfs(r+1,c,cur),dfs(r-1,c,cur),dfs(r,c+1,cur),dfs(r,c-1,cur))
+            return cache[(r,c)]
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                dfs(i,j,-1)
+        
+        return max(cache.values())
