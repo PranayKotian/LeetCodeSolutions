93f4b3f6e4e8bae2796fb2cc86d507ce49aa840f
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-06 11:18:43 -0500

    Time: 148 ms (98.65%), Space: 18 MB (71.59%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,48 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        #Solution 3: Dynamic Programming
+        #Time: O(n*sum(nums)) Space: O(sum(nums))
+        if sum(nums)%2 == 1:
+            return False
+        
+        target = sum(nums)//2
+        sums = set([0, nums[0]])
+        
+        for n in nums[1:]:
+            for s in sums.copy():
+                sums.add(s+n)
+            if target in sums:
+                return True
+        
+        return False
+        
+        #Solution 2: Backtrack w/ Cache
+        #Time: O(n*sum(nums)) Space:
+        #where sum(nums) is (max) 200*100 = 20000
+        
+        
+        """
+        #Solution 1: Brute force DFS
+        #Time: O(2^n) Space: O(n)
+        #(Time Limited Exceeded w/ 36/141 passed) 
+        
+        if sum(nums)%2 == 1:
+            return False
+        
+        def dfs(cur, arr):
+            if sum(arr) == cur:
+                return True
+            if cur > sum(arr):
+                return False
+            
+            for i in range(len(arr)):
+                elm = arr.pop(0)
+                if dfs(cur+elm, arr):
+                    return True
+                arr.append(elm)
+            
+            return False
+        
+        return dfs(0, nums)
+        """
