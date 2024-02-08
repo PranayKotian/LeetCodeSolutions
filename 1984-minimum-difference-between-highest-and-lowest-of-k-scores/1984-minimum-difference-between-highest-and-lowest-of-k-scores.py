2aed9b7f4dd7d7955c0c1932c65e033fa133c222
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-08 00:05:58 -0500

    Time: 79 ms (97.39%), Space: 16.8 MB (77.93%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -2,6 +2,7 @@ class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         
         #Solution 1: Sorting
+        #Time: O(nlogn) Space: O(1)
         
         nums.sort()
         res = 10**6
