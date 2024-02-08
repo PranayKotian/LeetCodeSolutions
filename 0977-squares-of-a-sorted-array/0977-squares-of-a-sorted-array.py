01a9c03678b642aa8a51c3cc6741ea995f0dcb4e
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-08 13:33:20 -0500

    Time: 154 ms (77.04%), Space: 19 MB (51.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,11 +1,27 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         
+        #Solution 2: Binary Search Solution
+        #Time: O(n) Space: O(n)
         
+        res = []
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                res.append(nums[l]**2)
+                l+=1
+            else:
+                res.append(nums[r]**2)
+                r-=1
         
+        return res[::-1]
+        
+        """
         #Solution 1: .sort() solution
         #Time: O(nlogn) Space: O(n)
         
         nums = [i**2 for i in nums]
         nums.sort()
         return nums
+        """
