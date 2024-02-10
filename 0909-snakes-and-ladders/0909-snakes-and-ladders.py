03f8e8ba11e41eb6038eaaf18daac7d2036132f1
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-10 11:53:51 -0500

    Time: 86 ms (96.98%), Space: 16.7 MB (80.04%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,7 +1,7 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         
-        #Solution 1: Dyanmic Programming + BFS
+        #Solution 1: BFS (Shortest Path Problem)
         #Time: O(n^2) Space: O(n^2)
         
         n = len(board)
@@ -16,15 +16,17 @@ class Solution:
         minmoves = [21**2 for i in range(n**2)]
         minmoves[0] = 0
         
-        def explore(i):
-            for j in range(i+1, min(i+7,n**2)):
-                if newboard[j] != -1:
-                    j = newboard[j]-1
-                if minmoves[i]+1 < minmoves[j]:
-                    minmoves[j] = minmoves[i]+1
-                    explore(j)
+        visit = set([0])
+        q = deque([0])
         
-        explore(0)
+        while q:
+            for i in range(len(q)):
+                i = q.popleft() 
+                for j in range(i+1, min(i+7,n**2)):
+                    if newboard[j] != -1: j = newboard[j]-1
+                    if j not in visit: q.append(j)
+                    minmoves[j] = min(minmoves[j], minmoves[i]+1)
+                    visit.add(j)
+                    if j == n**2-1: return minmoves[j]
         
-        if minmoves[-1] == 21**2: return -1
-        return minmoves[-1]
+        return -1
