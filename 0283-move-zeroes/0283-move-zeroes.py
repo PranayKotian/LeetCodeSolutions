9ff679886c84eea90f92f201662b2502651e2147
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-11 14:23:28 -0500

    Time: 120 ms (90.27%), Space: 18.1 MB (81.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,6 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #Solution 2: Two Pointer Solution (w/ Swapping)
+        #Time: O(n) Space: N/A
+        
+        l = 0
+        for r in range(len(nums)): 
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+        
+        """
         #Solution 1: Two Pointer Solution
         #Time: O(n) Space: N/A 
         
@@ -18,3 +28,7 @@ class Solution:
         while i < len(nums):
             nums[i] = 0
             i += 1
+            
+        #Solution 0: Extra Memory Solution
+        #Time: O(n) Space: O(n)
+        """
