4ae1b95f7af9282c9168826130b448ba0560635c
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-12 10:47:50 -0500

    Time: 63 ms (68.57%), Space: 23.2 MB (71.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,6 +1,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         
+        #Solution 2: bin count solution
+        #Time: O(32n) Space: O(n)
+        
+        #bin() converts integer to binary representation (str) 
+        return [bin(i).count('1') for i in range(n+1)]
+        
+        """
         #Solution 1: Counting bits for each number
         #Time: O(32n) Space: O(n)
         
@@ -11,3 +18,4 @@ class Solution:
                 ans[i] += temp%2
                 temp = temp >> 1
         return ans
+        """
