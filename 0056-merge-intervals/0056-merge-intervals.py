60414fa65a2ff841bd0748f224a313edb9c43cdf
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-17 08:59:41 -0500

    Time: 137 ms (19.43%), Space: 21.2 MB (51.06%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        #Solution 1: Sort Intervals and Merge Solution
+        #Time: O(nlogn) Space: O(n)
+        
+        intervals.sort()
+        res = []
+        cur = intervals[0]
+        for i in range(1,len(intervals)):
+            curi = intervals[i]
+            if cur[1] < curi[0]:
+                res.append(cur)
+                cur = curi
+            cur = [min(cur[0], curi[0]), max(cur[1], curi[1])]
+        res.append(cur)
+        return res
