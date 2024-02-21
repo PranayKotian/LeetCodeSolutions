885165fa64abbaeaee5ca07f8d7ec99e1d769bbd
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-21 00:39:56 -0500

    Time: 115 ms (51.92%), Space: 18.6 MB (16.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
+        #Solution 2: Bit Manipulation Solution
+        #Time: O(n) Space: O(1)
+        
+        
+        """
         #Solution 1: Math Solution
         #Time: O(n) Space: O(1)
         
@@ -9,3 +14,13 @@ class Solution:
         for i in nums: 
             total -= i
         return total
+        """
+        
+        #Solution 0: Bag Solution
+        #Time: O(n) Space: O(n)
+        
+        bag = set([i for i in range(len(nums)+1)])
+        for n in nums:
+            bag.remove(n)
+        return bag.pop()
+        
