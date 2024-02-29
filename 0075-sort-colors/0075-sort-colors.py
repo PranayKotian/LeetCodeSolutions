2e0043d123d06f11a09bf6304bd5391b39f80991
Pranay Kotian <lohitkotian@yahoo.ca>
2024-02-29 13:28:31 -0500

    Time: 38 ms (55.18%), Space: 16.5 MB (93.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,9 +4,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
+        #Solution 3: Bucket Sort Solution
+        #optimal since we know there are only 3 possible values therefore 3 possible buckets
+        #Time: O(n) Space: O(n)
+        
+        c = {i:0 for i in range(4)}
+        for n in nums:
+            c[n] += 1
+        start,end = 0,c[0]
+        for i in range(3):
+            for a in range(start,end):
+                nums[a] = i
+            start,end = end,end+c[i+1]
+        
+        """
         #Solution 2: Real Bubble Sort
-        #Time: O(n^2)   Space: O(n)
-        #Time: 61%      Space: 35%
+        #Time: O(n^2)   Space: N/A
         
         n = len(nums)
         for i in range(n-1):
@@ -17,10 +30,10 @@ class Solution:
                     run = True
             if not run:
                 break
-        
+        """
         """
         #Solution 1: Inefficient Bubble Sort 
-        #Time: O(n^2)   Space: O(n)
+        #Time: O(n^2)   Space: N/A
         #Time: 5%       Space: 62%
         
         run = True
