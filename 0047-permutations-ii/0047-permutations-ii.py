d3b5de0ff5fc67d7fa7cdb788f696fbd2350f7fe
Pranay Kotian <lohitkotian@yahoo.ca>
2024-03-01 09:52:37 -0500

    Time: 618 ms (9.60%), Space: 16.9 MB (68.18%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -2,15 +2,12 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         
         #Solution 1: Permutations + Remove Duplicates (Inefficient Solution)
-        #Time:      Space: 
+        #Time: O(n^k)   Space: O(n!)
         
         res = []
-        
         def permute(left, cur):
             if left == []:
-                if cur not in res: res.append(cur)
-                return
-            
+                if cur not in res: res.append(cur)            
             for n in left.copy():
                 left.remove(n)
                 permute(left, cur+[n])
