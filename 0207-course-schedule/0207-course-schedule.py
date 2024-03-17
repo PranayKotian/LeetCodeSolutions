a9d9b123d32e4fc4eed0931a33e540e84218a276
Pranay Kotian <lohitkotian@yahoo.ca>
2024-03-16 23:34:28 -0400

    Time: 96 ms (27.31%), Space: 18.2 MB (96.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #Recursive DFS
-        reqs = {i:[] for i in range(numCourses)}
-        for c,p in prerequisites:
-            reqs[c].append(p)
+        
+        #Solution 1: Adjacency List Solution
+        #Time: O(n + p) Space: O(1)
+        
+        prereq = {i:[] for i in range(numCourses)}
+        for p in prerequisites:
+            prereq[p[0]] += [p[1]]
         
         visited = set()
-        def dfs(crs):
-            if crs in visited:
-                return False
-            if reqs[crs] == []:
+        
+        def dfs(n):
+            if prereq[n] == []:
                 return True
+            if n in visited:
+                return False
             
-            visited.add(crs)
-            for pre in reqs[crs]:
-                if dfs(pre) == False: return False
-            visited.remove(crs)
-            reqs[crs] = []
-            
+            visited.add(n)
+            for p in prereq[n]:
+                if not dfs(p):
+                    return False
+            prereq[n] = []
+            visited.remove(n)
             return True
         
-        for crs in reqs:
+        for crs in range(numCourses):
             if dfs(crs) == False: return False
         return True
