51feec71d2019490c031139bc9c5d38929c205ab
Pranay Kotian <lohitkotian@yahoo.ca>
2024-08-14 11:57:55 -0400

    Time: 68 ms (11.87%), Space: 16.4 MB (78.65%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,28 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        
+        #One pass solution
+        #Time: O(n) Space: O(1)
+        
+        mins = [sys.maxsize, sys.maxsize]
+        for p in prices:
+            if p < mins[0]:
+                mins[0], mins[1] = p, mins[0]
+            elif p < mins[1]:
+                mins[1] = p
+
+        if sum(mins) > money:
+            return money
+        else:
+            return money - sum(mins)
+        
+        """
+        #Sorting solution
+        #Time: O(nlogn) Space: O(1)
+        
+        prices.sort()
+        if prices[0] + prices[1] > money:
+            return money
+        else:
+            return money - prices[0] - prices[1]
+        """
