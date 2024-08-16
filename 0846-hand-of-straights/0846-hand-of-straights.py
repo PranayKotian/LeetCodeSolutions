efa74de822f5da13e15741d2f02fdb9606e42840
Pranay Kotian <lohitkotian@yahoo.ca>
2024-08-16 00:46:35 -0400

    Time: 702 ms (8.10%), Space: 18 MB (97.01%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,42 +1,20 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        #Efficient solution?
-        #Time: O(n) Space: O(n)
+        
+        #Inefficient Solution
+        #Time: O(n^2) Space: O(1)
         
         if len(hand) % groupSize != 0:
             return False
         
-        count = {}
-        for n in hand:
-            count[n] = count.get(n,0) + 1
-        
-        while count:
-            minVal = min(count.keys())
-            for i in range(groupSize):
-                if minVal not in count:
-                    return False
-                count[minVal] -= 1
-                if count[minVal] == 0:
-                    del count[minVal]
-                minVal += 1
-        
-        return True
-        
-        """
-        #Inefficient solution
-        #Time: O(n^2) Space: O(n)
-        if len(hand) % groupSize != 0:
-            return False
-        
-        #O(nlogn) operation
+        #Smallest values will always need consecutive numbers
         hand.sort()
-        
         while hand:
-            minVal = hand.pop(0)
-            for i in range(groupSize-1):
-                minVal += 1
-                if minVal not in hand:
+            i = hand[0]
+            for j in range(1, groupSize):
+                if i+j not in hand:
                     return False
-                hand.remove(minVal) #O(n) operation
-        return True
-        """
+            hand.remove(i)
+            for j in range(1, groupSize):
+                hand.remove(i+j)
+        return True 
