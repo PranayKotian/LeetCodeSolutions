fe5745a3bb05944fd3824786e53dd0754e91ef15
Pranay Kotian <lohitkotian@yahoo.ca>
2024-08-26 15:09:51 -0400

    Time: 91 ms (53.51%), Space: 18.6 MB (12.53%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,6 +1,42 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
+        #Topological Sort
+        #Time: O(n+p) Space: O(n)
+        
+        #Create adjacency list
+        prereqs = {i:[] for i in range(numCourses)}
+        for crs,prereq in prerequisites:
+            prereqs[crs].append(prereq)
+        
+        ordering = []
+        
+        visited = set()
+        completed = set()
+        
+        def dfs(course):
+            if course in visited:
+                return False
+            if course in completed:
+                return True
+            
+            visited.add(course)
+            for req in prereqs[course]:
+                if dfs(req) == False:
+                    return False
+            visited.remove(course)
+            completed.add(course)
+            ordering.append(course)
+            prereqs[course] = []
+            return True
+        
+        for crs in prereqs:
+            if dfs(crs) == False:
+                return []
+        
+        return ordering
+        
+        """
         #Adjacency List + Inefficient BFS
         #Time: O(?) Space: O(n)
         
@@ -39,3 +75,4 @@ class Solution:
         if change == False:
             return []
         return ordering
+        """
