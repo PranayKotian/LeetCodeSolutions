a9be1916418260139da373faee57315fbf24b607
Pranay Kotian <lohitkotian@yahoo.ca>
2024-08-26 14:45:16 -0400

    Time: 567 ms (5.06%), Space: 18.4 MB (61.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,27 +1,41 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        #Topological sort solution (BFS)
-        req = {i:set() for i in range(numCourses)} #prereq: list of next courses
-        indegree = numCourses*[0]
         
-        for crs,pre in prerequisites:
-            req[pre].add(crs)
-            indegree[crs] += 1
+        #Adjacency List + Inefficient BFS
+        #Time: O(?) Space: O(n)
         
-        q = deque()
-        for c in range(numCourses):
-            if indegree[c] == 0:
-                q.append(c)
+        #Create adjacency list
+        adjacent = {}
+        for crs,prereq in prerequisites:
+            if crs not in adjacent:
+                adjacent[crs] = set()
+            adjacent[crs].add(prereq)
         
-        res = []
-        while q:
-            cur = q.pop()
-            res.append(cur)
-            for nextCrs in req[cur]:
-                indegree[nextCrs] -= 1
-                if indegree[nextCrs] == 0:
-                    q.append(nextCrs)
+        courses = set([i for i in range(numCourses)])
+        ordering = []
+        change = True
         
-        if len(res) == numCourses:
-            return res
-        return []
+        while courses and change:
+            change = False
+            
+            #Add to ordering
+            for i in courses.copy():
+                if i not in adjacent:
+                    courses.remove(i)
+                    ordering.append(i)
+                    change = True
+            
+            #Remove from adjacency list
+            for k in adjacent:
+                for i in adjacent[k].copy():
+                    if i not in courses:
+                        adjacent[k].remove(i)
+            
+            #Remove empty lists
+            for k in adjacent.copy():
+                if adjacent[k] == set():
+                    del adjacent[k]
+        
+        if change == False:
+            return []
+        return ordering
