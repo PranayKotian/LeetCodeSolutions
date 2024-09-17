cd4a72cad8c89755c245a31ba30d2039d90dd162
Pranay Kotian <lohitkotian@yahoo.ca>
2024-09-16 22:48:54 -0400

    Time: 42 ms (17.08%), Space: 16.4 MB (66.39%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,22 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
+        #Simple Solution
+        #Time: O(n) Space: O(1)
+        
+        idx = len(digits) - 1
+        while idx >= 0 and digits[idx] == 9:
+            digits[idx] = 0
+            idx -= 1
+        
+        if idx == -1:
+            digits = [1] + digits
+        else:
+            digits[idx] += 1
+        return digits
+        
+        
+        """
         #Convert To Int Solution
         #Time: O(n) Space: O(n)
         
@@ -10,3 +26,4 @@ class Solution:
             total += digits[i] * 10**(n-i-1)
         total += 1
         return [int(i) for i in str(total)]
+        """
