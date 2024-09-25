e758150c0bb506f510ebab30d06ad074c916c608
Pranay Kotian <lohitkotian@yahoo.ca>
2024-09-25 01:52:36 -0400

    Time: 1880 ms (22.95%), Space: 17.1 MB (13.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,48 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        #Solution 3: DP solution
+        #DP Solution
         #Time: O(n^2) Space: O(n)
         
-        LIS = [1 for i in range(len(nums))]
+        arr = [1 for i in range(len(nums))]
         
-        for i in range(len(nums)-2,-1,-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1+LIS[j])
-
-        return max(LIS)
+        for i in range(len(nums)):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    arr[i] = max(arr[i], arr[j]+1)
         
-        """
-        #Solution 2: Brute force w/ caching
-        #Time: O(n^2) Space: O(n*1e8)
-        #(Memory Limit Exceeded)
-        
-        cache = {}
-        def dfs(prev, i):
-            if i == len(nums):
-                return 0
-            if (prev, i) not in cache:
-                if nums[i] > prev:
-                    cache[(prev,i)] = max(1 + dfs(nums[i], i+1), dfs(prev, i+1))
-                else:
-                    cache[(prev,i)] = dfs(prev, i+1)
-            return cache[(prev,i)]
-        
-        return dfs(-10**5, 0)
-        """
-        
-        """
-        #Solution 1: Brute force
-        #Time: O(2^n) Space: O(n) (max recursive stack depth)
-        
-        def dfs(prev, i):
-            if i == len(nums):
-                return 0
-            if nums[i] > prev:
-                return max(1 + dfs(nums[i], i+1), dfs(prev, i+1))
-            else:
-                return dfs(prev, i+1)
-                    
-        return dfs(-10**5, 0)
-        """
+        return max(arr)
