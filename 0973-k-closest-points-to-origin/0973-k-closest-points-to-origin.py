ae3a88bd527709f47d24618c16ae5d1cf57b23c5
Pranay Kotian <lohitkotian@yahoo.ca>
2024-09-30 13:28:57 -0400

    Time: 664 ms (10.68%), Space: 23 MB (40.82%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,19 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        #Minheap solution
-        #Time: O(klogn) Space: O(n)
+        #Min Heap Solution - Store distances with coordinates
+        #Time: O(nlogn) Space: O(n)
         
         minHeap = []
-        #Calculates all euclidean distances
-        for x,y in points:
-            minHeap.append((math.sqrt(x**2 + y**2), x, y))
-        heapq.heapify(minHeap) #O(n) operation
+        heapq.heapify(minHeap)
         
-        #Pops k coords from heap 
-        #O(k logn) operation
+        for x,y in points:
+            heapq.heappush(minHeap, [(x**2 + y**2)**0.5, [x,y]])
+        
         res = []
         for i in range(k):
-            dist,x,y = heapq.heappop(minHeap)
-            res.append([x,y])
+            res.append(minHeap[0][1])
+            heapq.heappop(minHeap)
         return res
