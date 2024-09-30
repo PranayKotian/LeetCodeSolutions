10e542917bd74b474592c38a2a0fbfdc3dab846c
Pranay Kotian <lohitkotian@yahoo.ca>
2024-09-30 14:16:46 -0400

    Time: 339 ms (99.52%), Space: 17.3 MB (16.37%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -4,15 +4,7 @@ class Solution:
         #Counter Solution
         #Time: O(n) Space: O(n)
         
-        c = Counter(tasks)
-        mostCommon = c.most_common()
-        a = mostCommon[0][1]
-        
-        b = 1
-        for let,f in mostCommon[1:]:
-            if f == a:
-                b += 1
-            else:
-                break
-        
+        c = Counter(tasks).values()
+        a = max(c)
+        b = list(c).count(a)
         return max(len(tasks), (n+1)*(a-1) + b)
