cf4454fa2e7451de0e632a8c367ab819f75a66c5
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-03 01:49:39 -0400

    Time: 757 ms (21.12%), Space: 30.6 MB (74.67%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,6 +1,19 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         
+        #One Pass Solution
+        #Time: O(n) Space: O(2)
+        
+        inc = True
+        dec = True
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                dec = False
+            elif nums[i] < nums[i-1]:
+                inc = False
+        return inc or dec
+        
+        """
         #Three Cases Solution
         #Time: O(n) Space: O(1)
         
@@ -17,3 +30,4 @@ class Solution:
                 if n != nums[0]:
                     return False
         return True
+        """
