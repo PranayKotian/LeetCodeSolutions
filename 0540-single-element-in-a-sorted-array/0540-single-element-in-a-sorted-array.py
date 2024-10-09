3ba349a8329e8bfd07a5ef981f7bc43e86b8f42a
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-09 12:42:50 -0400

    Time: 147 ms (19.06%), Space: 26.8 MB (16.75%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,6 +1,29 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         
+        #Binary Search Solution
+        #Time: O(logn) Space: O(1)
+        
+        l = 0
+        r = len(nums)-1
+        m = (l+r)//2
+        
+        while l <= r:
+            if m == 0 or m == len(nums)-1 or (nums[m-1] != nums[m] and nums[m+1] != nums[m]):
+                return nums[m]
+            elif nums[m-1] == nums[m]:
+                if m%2 == 0:
+                    r = m-2
+                else:
+                    l = m+1
+            else: #nums[m+1] == nums[m]
+                if m%2 == 0:
+                    l = m+2
+                else:
+                    r = m-1
+            m = (l+r)//2
+        
+        """
         #Bit Manipulation Solution
         #Time: O(n) Space: O(1)
         
@@ -8,3 +31,4 @@ class Solution:
         for n in nums: 
             ans ^= n
         return ans
+        """
