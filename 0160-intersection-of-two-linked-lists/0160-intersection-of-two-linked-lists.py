ef1da06212ce9b58c06067f8fb222e6e5fe67aee
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-10 13:08:53 -0400

    Time: 87 ms (23.55%), Space: 27.2 MB (60.18%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -7,6 +7,44 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
+        #Elegant Constant Memory Solution
+        #Time: O(a+b) Space: O(1)
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1 #will either return intersection point or both lists will have gone to null
+        
+        """
+        #Constant Memory Solution
+        #Time: O(a+b) Space: O(1)
+        
+        a = headA
+        b = headB
+        aLen = bLen = 1
+        while headA.next:
+            aLen += 1
+            headA = headA.next
+        while headB.next:
+            bLen += 1
+            headB = headB.next
+        if headA != headB:
+            return None
+        
+        if aLen > bLen:
+            for i in range(aLen-bLen):
+                a = a.next
+        elif bLen > aLen:
+            for i in range(bLen-aLen):
+                b = b.next
+        
+        while a != b:
+            a = a.next
+            b = b.next
+        return a 
+        """
+        
+        """
         #Set Solution
         #Time: O(a+b) Space: O(a)
         
@@ -20,3 +58,4 @@ class Solution:
                 return headB
             headB = headB.next
         return None
+        """
