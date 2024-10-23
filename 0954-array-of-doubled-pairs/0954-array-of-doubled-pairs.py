a26a31e5f4d43090bd2fa8671046716eb4b2c317
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-23 03:41:30 -0400

    Time: 151 ms (94.27%), Space: 19.5 MB (6.90%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
@@ -1,6 +1,20 @@
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
         
+        #Dictionary Solution Optimized
+        #Time: O(nlogn) Space: O(n)
+        
+        table = Counter(arr)
+        for n in sorted(arr, key=abs):
+            if table[n] == 0:
+                continue
+            if table[n*2] == 0:
+                return False
+            table[n] -= 1
+            table[n*2] -= 1
+        return True
+    
+        """
         #Dictionary Solution
         #Time: O(nlogn) Space: O(n)
         
@@ -21,3 +35,4 @@ class Solution:
                 table[n] -= 1
                 table[n*2] -= 1
         return True
+        """
