d4e289dbb3045080dc654fc792977cb20f3c388e
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-27 09:51:22 -0400

    Time: 75 ms (76.97%), Space: 22 MB (98.14%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,6 +1,30 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
+        #Three Pointer Solution
+        #Time: O(n) Space: O(1)
+        
+        res = 0
+        odds = 0
+        l = m = 0
+        
+        for r in range(len(nums)):
+            if nums[r]%2 == 1:
+                odds += 1
+            
+            while odds > k:
+                if nums[l]%2 == 1:
+                    odds -= 1
+                l += 1
+            m = l
+            
+            if odds == k:
+                while nums[m]%2 == 0:
+                    m += 1
+                res += m-l+1
+        return res
+        
+        """
         #Two Pointer Solution
         #Time: O(n) Space: O(1)
         
@@ -29,3 +53,4 @@ class Solution:
             odds -= 1
         
         return res
+        """
