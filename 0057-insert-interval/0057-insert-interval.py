cbbb5cf40d51188d6b6a2700816b70d8c7f993c3
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-27 22:15:42 -0400

    Time: 4 ms (21.27%), Space: 18.9 MB (90.31%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,6 +1,9 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
+        #Compact Solution
+        #Time: O(n) Space: O(n)
+        
         sn,en = newInterval
         res = []
         inserted = False
@@ -8,15 +11,9 @@ class Solution:
         for s,e in intervals:
             if e < sn:
                 res.append([s,e])
-            elif (s<=sn<=e and s<=en<=e): #interval to insert is in existing interval
-                inserted = True
-                res.append([s,e])
-            elif sn<=s<=en and sn<=e<=en: #existing interval is in interval to insert
-                continue
-            elif sn<=s<=en:
-                en = e
-            elif sn<=e<=en:
-                sn = s
+            elif s<=sn<=e or s<=en<=e or sn<=s<=en or sn<=e<=en:
+                sn = min(sn,s)
+                en = max(en,e)
             if s > en: 
                 if not inserted:
                     res.append([sn,en])
