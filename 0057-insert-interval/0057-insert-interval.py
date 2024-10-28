343f36a52ba71d8e15a128550071fbd98fa0def8
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-27 22:08:49 -0400

    Time: 2 ms (57.02%), Space: 18.8 MB (95.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,18 +1,29 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        #Solution 1: Add As We Go
-        #Time: O(n) Space: O(n)
-        
+        sn,en = newInterval
         res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-        res.append(newInterval)
+        inserted = False
+        
+        for s,e in intervals:
+            if e < sn:
+                res.append([s,e])
+            elif (s<=sn<=e and s<=en<=e): #interval to insert is in existing interval
+                inserted = True
+                res.append([s,e])
+            elif sn<=s<=en and sn<=e<=en: #existing interval is in interval to insert
+                continue
+            elif sn<=s<=en:
+                en = e
+            elif sn<=e<=en:
+                sn = s
+            if s > en: 
+                if not inserted:
+                    res.append([sn,en])
+                    inserted = True
+                res.append([s,e])
+        
+        if not inserted:
+            res.append([sn,en])
         
         return res
