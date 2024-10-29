99ba492a9cd2d10622e1b2a57fcafc0cc32dbdaf
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-29 09:59:01 -0400

    Time: 35 ms (36.29%), Space: 24.2 MB (97.83%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -7,7 +7,6 @@ class Solution:
         res = []
         for n in nums:
             nums[abs(n)-1] = abs(nums[abs(n)-1])*-1
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                res.append(i+1)
-        return res
+        return [i+1 for i in range(len(nums)) if nums[i] > 0]
+    
+    
