b441499994d1a49dc35fec95b640f3e5027c706a
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-29 13:19:14 -0400

    Time: 0 ms (100.00%), Space: 17.7 MB (15.43%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,8 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        #Iterative Solution - DFS
+        #Time: O(n) Space: O(n)
+        
+        stack = [[root, targetSum]]
+        while stack:
+            node,tar = stack.pop()
+            if node is None:
+                continue
+            if node.left is None and node.right is None and node.val == tar:
+                return True
+            stack.append([node.left, tar-node.val])
+            stack.append([node.right, tar-node.val])
+        return False
+        
+        """
+        #Recursive Solution
+        #Time: O(n) Space: O(h) 
+        
         if root is None:
             return False
         if root.left is None and root.right is None:
             return root.val == targetSum
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        """
