97f1bc52687b4c781b33d87078f54eaabc57d210
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-29 10:18:59 -0400

    Time: 55 ms (22.45%), Space: 17.6 MB (96.69%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,34 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         
-        #Solution 3: DP Solution
-        #Time: O(n) Space: O(n) 
+        #Brute Force Solution
+        #Time: O(nlogn) Space: O(1)
         
-        ans = [0] * (n+1)
-        offset = 1
-        for i in range(1,n+1):
-            if offset*2 == i:
-                offset = i
-            ans[i] = 1+ans[i-offset]
-        return ans
-        
-        """
-        #Solution 2: bin().count() List Comprehension Solution
-        #Time: O(nlogn) Space: O(n)
-        
-        #bin() converts integer to binary representation (str) 
-        return [bin(i).count('1') for i in range(n+1)]
-        """
-        
-        """
-        #Solution 1: Counting Bits Individually Solution
-        #Time: O(nlogn) Space: O(n)
-        
-        ans = [0 for i in range(n+1)]
+        res = []
         for i in range(n+1):
-            temp = i
-            while temp:
-                ans[i] += temp%2
-                temp = temp >> 1
-        return ans
-        """
+            ones = 0
+            while i!=0:
+                ones += i%2
+                i = i//2
+            res.append(ones)
+        return res 
