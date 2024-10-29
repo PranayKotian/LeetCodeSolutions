ac053188e4ad88a9437dd1617d8de5bd446e065c
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-29 13:14:20 -0400

    Time: 0 ms (100.00%), Space: 17.7 MB (7.70%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,33 +6,8 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        #Solution 2: Recursive Solution w/ Helper Function
-        #Time: O(n) Space: O(n) or O(logn) if balanced tree
-        
         if root is None:
             return False
-        
-        def dfs(node, cur):
-            if node == None:
-                return False
-            cur += node.val
-            if node.left is None and node.right is None:
-                return cur == targetSum
-            return dfs(node.left, cur) or dfs(node.right, cur)
-        
-        return dfs(root, 0)
-        
-        """
-        #Solution 1: Recursive Solution
-        #Time: O(n) Space: O(n) or O(logn) if balanced tree
-        
-        #Base Cases
-        if root == None:
-            return False
-        if root.val == targetSum and root.left is None and root.right is None:
-            return True
-        
-        #Recursive Case
+        if root.left is None and root.right is None:
+            return root.val == targetSum
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
-        """
