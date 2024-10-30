118a38490d8d02ec9aff74028af5ad3d3570cc29
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-29 22:11:54 -0400

    Time: 0 ms (100.00%), Space: 16.6 MB (74.72%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,10 +4,13 @@ class Solution:
         #Two Pointer Solution
         #Time: O(n) Space: O(1)
         
+        if len(s) == 0: return True
+        if len(s) > len(t): return False
+        
         s1 = 0
         for c in t:
-            if s1 == len(s):
-                return True
             if s[s1] == c:
                 s1 += 1
-        return s1 == len(s)
+                if s1 == len(s):
+                    return True
+        return False
