fe422fd5ecf2d9d0ec9056ec9d59002e9ff53999
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-30 20:08:42 -0400

    Time: 95 ms (70.65%), Space: 27.5 MB (98.72%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         
-        #Sliding Window
+        #Two Pointer Solution
         #Time: O(n) Space: O(1)
         
         res = 0
         l = 0 
         r = len(height)-1
-        
         while l < r:
             res = max(res, (r-l)*min(height[r],height[l]))
             if height[l] > height[r]:
