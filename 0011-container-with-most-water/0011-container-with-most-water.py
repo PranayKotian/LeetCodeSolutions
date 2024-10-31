5f7d588411ed3092b3aa8eef5f2c6589a6de8ddf
Pranay Kotian <lohitkotian@yahoo.ca>
2024-10-30 20:07:30 -0400

    Time: 96 ms (65.69%), Space: 27.6 MB (94.44%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        #Sliding Window
+        #Time: O(n) Space: O(1)
+        
+        res = 0
+        l = 0 
+        r = len(height)-1
+        
+        while l < r:
+            res = max(res, (r-l)*min(height[r],height[l]))
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return res
+        
+        #Brute Force Solution
+        #Time: O(n^2) Space: O(1)
+        #Check all combinations of containers
