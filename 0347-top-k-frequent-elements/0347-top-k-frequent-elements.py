369292a4f0b36e3ae7505270b6c976b6cb995071
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-01 22:05:25 -0400

    Time: 0 ms (100.00%), Space: 20.3 MB (89.13%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        #Solution 1: Python Counter Solution
-        #Time: O(n) Space: O(n)
+        #Counter + Heap Solution
+        #Time: O(n+klogn) Space: O(n)
         
         c = Counter(nums)
-        c = [i for i,j in c.most_common()][:k]
-        return c
+        heap = []
+        for i in c:
+            heap.append([-c[i], i])
+        heapq.heapify(heap)
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
