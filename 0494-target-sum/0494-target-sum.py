a42ecbc4347147b898f8baed542d8f21916618e8
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-06 11:29:34 -0500

    Time: 132 ms (72.24%), Space: 44.3 MB (6.86%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,19 +1,38 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        #Brute Force w/o Memoization
-        #Time: O(2^n) Space: O(n)
-        #TLE - 97/140
+        #Brute Force w/ Memoization
+        #Time: O(n*sum(nums)) Space: O(n*sum(nums))
         
-        cache = {}
+        # cache = {}
+        @cache
         def dfs(i,tot):
             if i == len(nums):
                 if tot == target:
                     return 1
                 return 0
+            return dfs(i+1, tot+nums[i]) + dfs(i+1, tot-nums[i])
             
-            if (i+1,tot+nums[i]) not in cache: cache[(i+1,tot+nums[i])] = dfs(i+1, tot+nums[i])
-            if (i+1,tot-nums[i]) not in cache: cache[(i+1,tot-nums[i])] = dfs(i+1, tot-nums[i])
-            return cache[(i+1,tot+nums[i])] + cache[(i+1,tot-nums[i])]
+            # if (i+1,tot+nums[i]) not in cache: cache[(i+1,tot+nums[i])] = dfs(i+1, tot+nums[i])
+            # if (i+1,tot-nums[i]) not in cache: cache[(i+1,tot-nums[i])] = dfs(i+1, tot-nums[i])
+            # return cache[(i+1,tot+nums[i])] + cache[(i+1,tot-nums[i])]
         
         return dfs(0,0)
+        
+        """
+        #Brute Force w/o Memoization
+        #Time: O(n^2) Space: O(n)
+        #TLE - 97/140
+        
+        self.res = 0
+        def dfs(i,tot):
+            if i == len(nums):
+                if tot == target:
+                    self.res += 1
+                return
+            dfs(i+1, tot+nums[i])
+            dfs(i+1, tot-nums[i])
+        
+        dfs(0,0)
+        return self.res
+        """
