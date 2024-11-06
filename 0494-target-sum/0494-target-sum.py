f4d34cb58448ae6b1be6be027d12bee12274a3e5
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-06 11:24:17 -0500

    Time: 229 ms (13.02%), Space: 43.9 MB (10.45%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,41 +1,19 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        #Recursive solution with caching
-        #Time: O(n*t) Space: O(n*t)
-        #(where t = all possible expression sums from the nums array)
+        
+        #Brute Force w/o Memoization
+        #Time: O(2^n) Space: O(n)
+        #TLE - 97/140
         
         cache = {}
-        def explore(i, curTar):
+        def dfs(i,tot):
             if i == len(nums):
-                if curTar == 0:
+                if tot == target:
                     return 1
-                else:
-                    return 0
-            if (i,curTar) in cache:
-                return cache[(i,curTar)]
-
-            cache[(i,curTar)] = explore(i+1, curTar+nums[i]) + explore(i+1, curTar-nums[i])
-            return cache[(i,curTar)]
-        
-        return explore(0, target)
-        
-        """
-        #Recursive brute force solution w/o caching
-        #Try every possible combination of +/- on each number in num
-        #Check if resulting expression equates to target
-        #Time: O(2^n) Space: O(1)
-        #(Time Limited Exceeded)
-        
-        if len(nums) == 1:
-            if nums[0] == 0 and target == 0:
-                return 2
-            if nums[0] == target or nums[0] == -target:
-                return 1
-            else:
                 return 0
+            
+            if (i+1,tot+nums[i]) not in cache: cache[(i+1,tot+nums[i])] = dfs(i+1, tot+nums[i])
+            if (i+1,tot-nums[i]) not in cache: cache[(i+1,tot-nums[i])] = dfs(i+1, tot-nums[i])
+            return cache[(i+1,tot+nums[i])] + cache[(i+1,tot-nums[i])]
         
-        p1 = self.findTargetSumWays(nums[1:], target+nums[0])
-        p2 = self.findTargetSumWays(nums[1:], target-nums[0])
-        
-        return p1 + p2
-        """
+        return dfs(0,0)
