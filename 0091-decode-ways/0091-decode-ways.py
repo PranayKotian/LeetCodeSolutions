bba63ca23bfd6c4efc4d9bc954e3521bc3cf3162
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 12:55:20 -0500

    Time: 7 ms (10.58%), Space: 17.4 MB (8.51%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,37 +1,10 @@
 class Solution:
+    @cache
     def numDecodings(self, s: str) -> int:
-        
-        #Dynamic Programming Solution
-        #Time: O(n) Space: O(1)
-        
-        validNums = set([str(i) for i in range(1,27)])
-        if s[0] not in validNums: 
+        if s == "":
+            return 1
+        if s[0] == "0":
             return 0
-        
-        arr = [1,1,0]
-        for i in range(1,len(s)):
-            if s[i] in validNums:
-                arr[2] += arr[1]
-            if s[i-1]+s[i] in validNums:
-                arr[2] += arr[0]
-            arr = [arr[1], arr[2], 0]
-        return arr[1]
-        
-        
-        """
-        #Dynamic Programming Solution - same solution but less clean
-        #Time: O(n) Space: O(1)
-        
-        valid = set([str(i) for i in range(1,27)])
-        c1, c2 = 1, 0
-        if s[0] in valid:
-            c2 = 1
-        for i in range(1, len(s)):
-            c3 = 0
-            if s[i] in valid:
-                c3 += c2
-            if s[i-1:i+1] in valid:
-                c3 += c1
-            c1, c2 = c2, c3
-        return c2
-        """
+        if s[:2] in set([str(i) for i in range(10,27)]):
+            return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
+        return self.numDecodings(s[1:])
