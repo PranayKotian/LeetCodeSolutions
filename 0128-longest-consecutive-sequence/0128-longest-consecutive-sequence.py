b89c59acf437155323a37be88122f1b480ca096b
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 11:51:13 -0500

    Time: 4072 ms (15.63%), Space: 33.4 MB (21.67%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,35 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
-        #Double Bag Solution? 
-        #O(2n) space, O(n) time
-        bag = set(nums)
-        starts = set()
-        
-        for i in nums:
-            if i-1 not in bag:
-                starts.add(i)
-        
-        maxVal = 0
-        for i in starts:
-            length = 1
-            while i+length in bag:
-                length += 1
-            maxVal = max(length, maxVal)
-        
-        return maxVal
-    
-        """
-        #Bag Solution
+        #Find Starts Solution
         #Time: O(n) Space: O(n)
         
+        numsSet = set(nums)
         res = 0
-        bag = set(nums)
         for n in nums:
-            if n-1 not in bag:
-                i = 1
-                while n+i in bag:
-                    i += 1
-                res = max(res,i)
+            if n-1 not in numsSet:
+                count = 0
+                while n in numsSet:
+                    count += 1
+                    n += 1
+                res = max(res,count)
         return res
-        """
