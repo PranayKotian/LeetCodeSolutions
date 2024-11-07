4dd25e8ae7b643384d7a43df2ec781104072b3b1
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 13:04:24 -0500

    Time: 1 ms (42.20%), Space: 16.7 MB (27.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,10 +1,19 @@
 class Solution:
-    @cache
     def numDecodings(self, s: str) -> int:
-        if s == "":
-            return 1
-        if s[0] == "0":
-            return 0
-        if s[:2] in set([str(i) for i in range(10,27)]):
-            return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
-        return self.numDecodings(s[1:])
+        
+        #DP Solution
+        #Time: O(n) Space: O(1)
+        
+        valid = set([str(i) for i in range(1,27)])
+        p1 = 1 if s[-1] in valid else 0
+        p2 = 1
+        
+        for i in range(len(s)-2,-1,-1):
+            if s[i] in valid:
+                if s[i:i+2] in valid:
+                    p1,p2 = p1+p2, p1
+                else:
+                    p1,p2 = p1,p1
+            else:
+                p1,p2 = 0,p1
+        return p1
