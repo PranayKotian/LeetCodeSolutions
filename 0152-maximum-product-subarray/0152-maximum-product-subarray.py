80b43d5c12a8703e74d23f72efe6bcf24e98fc8c
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 12:43:36 -0500

    Time: 7 ms (90.73%), Space: 17.2 MB (20.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,24 +1,18 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        #Brute force solution
-        #Time: O(n^2) Space: O(1)
-        
-        #Two pass solution
-        #Time: O(2n) Space: O(1)
-        
+        #Two Pass Solution
+        #Time: O(n) Space: O(1)
+
         res = nums[0]
-        cur = 1
-        for n in nums:
-            cur *= n
-            res = max(res,cur)
-            if cur == 0:
-                cur = 1
-        cur = 1
-        for n in nums[::-1]:
-            cur *= n
-            res = max(res,cur)
-            if cur == 0:
-                cur = 1
-        
+        prod = 0
+        for i in range(len(nums)):
+            if prod == 0: prod = nums[i]
+            else: prod *= nums[i]
+            res = max(res,prod)
+        prod = 0
+        for i in range(len(nums)-1,-1,-1):
+            if prod == 0: prod = nums[i]
+            else: prod *= nums[i]
+            res = max(res,prod)
         return res
