b61041979269f0a1e3080c64cf083a0c82e7805a
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 11:26:25 -0500

    Time: 3 ms (47.23%), Space: 18.8 MB (78.71%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,24 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        #Compacter Solution
+        #Insert Interval
         #Time: O(n) Space: O(n)
         
-        sn,en = newInterval
         res = []
-        
+        s2,e2 = newInterval
         for i in range(len(intervals)):
-            s = intervals[i][0]
-            e = intervals[i][1]
+            s1,e1 = intervals[i]
+            if e1 < s2:
+                res.append([s1,e1])
+                continue
+            if s1 > e2:
+                return res + [[s2,e2]] + intervals[i:]    
+            s2,e2 = min(s1,s2),max(e1,e2)
+        return res + [[s2,e2]]
             
-            if e < sn:
-                res.append([s,e])
-            elif s > en: 
-                res.append([sn,en])
-                return res + intervals[i:]
-            else:
-                sn = min(sn,s)
-                en = max(en,e)
-        
-        res.append([sn,en])
-        return res
+            
+            
+            
