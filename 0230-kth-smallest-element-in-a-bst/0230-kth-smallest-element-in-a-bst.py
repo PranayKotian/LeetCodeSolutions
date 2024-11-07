035218e5d553004ab9bd3d09ea4c7d6b4f9a3f8e
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-07 00:37:08 -0500

    Time: 0 ms (100.00%), Space: 20.2 MB (15.98%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,7 +7,27 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
-        #DFS Solution (add smallest values first)
+        #Iterative DFS Solution
+        #Time: O(n) Space: O(k)
+        
+        cur = root
+        stack = []
+        res = []
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            res.append(cur.val)
+            if k == 0:
+                return (res[-1])
+            cur = cur.right
+        return -1 #error
+        
+        """
+        #Recursive DFS Solution (add smallest values first)
         #Return when len(arr) == k
         #Time: O(n) Space: O(n)
         
@@ -20,3 +40,4 @@ class Solution:
             dfs(node.right)
         dfs(root)
         return res[k-1]
+        """
