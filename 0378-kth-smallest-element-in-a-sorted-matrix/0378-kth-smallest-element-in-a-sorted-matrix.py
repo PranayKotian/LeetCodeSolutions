83e3bb4fb8e878c6edb669391d017866990035f3
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-08 15:56:41 -0500

    Time: 30 ms (22.92%), Space: 20.3 MB (70.38%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,11 +1,38 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
+        #Heap Solution
+        #Time: O(klogn) Space: O(n)
+        
+        minHeap = []
+        for i in range(len(matrix)):
+            heappush(minHeap, (matrix[i][0], i, 0))
+        for i in range(k-1):
+            val,r,c = heappop(minHeap)
+            if c != len(matrix)-1:
+                heappush(minHeap, (matrix[r][c+1], r, c+1))
+        return minHeap[0][0]
+        
+        """
+        #MaxHeap Solution
+        #Time: O(n^2 * logk) Space: O(k)
+        
+        n = len(matrix)
+        maxHeap = []
+        for r in range(n):
+            for c in range(n):
+                heappush(maxHeap, -matrix[r][c])
+                if len(maxHeap) > k:
+                    heappop(maxHeap)
+        return -heappop(maxHeap)
+    
+        
         #MinHeap Solution
-        #Time: O(n + klogn) Space: O(n)
+        #Time: O(n^2 + klogn) Space: O(n^2)
         
         heap = [i for arr in matrix for i in arr]
-        heapq.heapify(heap)
+        heapify(heap)
         for i in range(k-1):
-            heapq.heappop(heap)
-        return heapq.heappop(heap)
+            heappop(heap)
+        return heappop(heap)
+        """
