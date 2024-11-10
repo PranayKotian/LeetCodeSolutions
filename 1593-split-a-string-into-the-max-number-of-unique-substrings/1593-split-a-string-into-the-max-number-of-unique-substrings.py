b6c1027e766a48176a3e39be6a08e009b64a7fc4
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-10 14:46:22 -0500

    Time: 127 ms (85.58%), Space: 16.6 MB (53.18%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,18 +1,31 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
         
-        #Backtracking Solution
-        #Time: Space:
+        #Brute Force Solution
+        #Time: O(2^n) Space: O(n)
         
-        def backtrack(bag, left):
-            if left == "":
-                return len(bag)
-            res = len(bag)
-            for i in range(1,len(left)+1):
-                if left[:i] not in bag:
-                    bag.add(left[:i])
-                    res = max(res, backtrack(bag, left[i:]))
-                    bag.remove(left[:i])
-            return res
+        '''
+        at each letter, choose to split at the end or skip
+        if substring in set, iteration is invalid
+        if index == len(s)-1, we split (including the final letter)
+        if index == len(s) return length of set
+        '''
+        
+        self.res = 1
+        
+        bag = set()
+        def backtrack(start,end):
+            if start == len(s):
+                self.res = max(self.res, len(bag))
+                return
             
-        return backtrack(set(), s)
+            sub = s[start:end]
+            if sub not in bag:
+                bag.add(sub)
+                backtrack(end, end+1)
+                bag.remove(sub)
+            if end != len(s): 
+                backtrack(start, end+1)
+            
+        backtrack(0,1)
+        return self.res
