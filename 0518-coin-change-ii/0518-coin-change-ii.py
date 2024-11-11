71eb976e5131cea04f59e56df1613789e3e4e3f6
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-10 23:52:08 -0500

    Time: 129 ms (77.29%), Space: 16.8 MB (69.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,32 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        #Most efficient DP solution
-        #Time: O(c*n) Space: O(n)
+        #DP Solution
+        #Time: O(n*c) Space: O(n)
         
-        arr = [0 for i in range(amount+1)]
-        arr[0] = 1
+        amounts_array = [0 for i in range(amount+1)]
+        amounts_array[0] = 1
         
-        #Potential combinations are found coin by coin
-        # to avoid repeated permutations
-        for c in coins: 
-            for i in range(1,amount+1):
-                if i-c >= 0:
-                    arr[i] += arr[i-c]
+        for c in coins:
+            for i in range(c, len(amounts_array)):
+                amounts_array[i] += amounts_array[i-c]
+        return amounts_array[-1]
+    
         
-        return arr[-1]
+        """
+        #Brute Force All Combinations
+        #Time: O(2^n) Space: O(n)
+        #TLE - 14/29
+        
+        self.res = 0
+        def backtrack(i: int, cur: int):
+            if cur == amount:
+                self.res += 1
+                return
+            if cur > amount or i == len(coins):
+                return
+            backtrack(i, cur+coins[i])
+            backtrack(i+1, cur)
+        backtrack(0, 0)
+        return self.res
+        """
