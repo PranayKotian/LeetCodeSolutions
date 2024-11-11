51992d708fd1035b647264deb727bd7d875e0739
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-11 00:06:44 -0500

    Time: 121 ms (87.54%), Space: 16.7 MB (83.21%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,6 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        #Brute Force w/ Caching
-        #Time: O(2^n) Space: O(n)
-        
-        @cache
-        def backtrack(i: int, cur: int):
-            if cur == amount:
-                return 1
-            if cur > amount or i == len(coins):
-                return 0 
-            return backtrack(i, cur+coins[i]) + backtrack(i+1, cur)
-        return backtrack(0, 0)
-        
-        """
         #DP Solution
         #Time: O(n*c) Space: O(n)
         
@@ -24,4 +11,18 @@ class Solution:
             for i in range(c, len(amounts_array)):
                 amounts_array[i] += amounts_array[i-c]
         return amounts_array[-1]
+        
+        """
+        #Brute Force w/ Caching
+        #Time: O(2^n), Space: O(n)
+        #Runtime: 1109 ms, Memory: 414 MB
+        
+        @cache
+        def backtrack(i: int, cur: int):
+            if cur == amount:
+                return 1
+            if cur > amount or i == len(coins):
+                return 0 
+            return backtrack(i, cur+coins[i]) + backtrack(i+1, cur)
+        return backtrack(0, 0)
         """
