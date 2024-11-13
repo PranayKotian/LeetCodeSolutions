2b9dbf79e324e5592c517066f53e6d65735c3de8
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-12 23:44:58 -0500

    Time: 3 ms (90.93%), Space: 17.5 MB (73.13%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -1,6 +1,29 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
+        #Two Pointer Solution
+        #Time: O(n+m) Space: O(min(m,n))
+        
+        res = []
+        p1 = p2 = 0
+        while p1 != len(firstList) and p2 != len(secondList):
+            s1,e1 = firstList[p1]
+            s2,e2 = secondList[p2]
+            
+            if s2 > e1:
+                p1 += 1
+            elif s1 > e2:
+                p2 += 1
+            else: #overlap
+                res.append([max(s1,s2), min(e1,e2)])
+                if e1 < e2:
+                    p1 += 1
+                else:
+                    p2 += 1
+        return res
+                    
+        
+        """
         #Brute Force Solution
         #Time: O(m*n) Space: O(min(m, n))
         #Compare all intervals from list1 to list2
@@ -14,3 +37,4 @@ class Solution:
                     break
                 res.append([max(s1,s2), min(e1,e2)])
         return res
+        """
