42cdf1296304281945ebbd74cb16897438071108
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-14 17:14:50 -0500

    Time: 0 ms (100.00%), Space: 16.5 MB (57.11%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,25 +1,18 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
 
-        #Wonky Stack Solution
+        #Stack Solution
         #Time: O(n) Space: O(n)
         
         stack = []
         for char in s:
             if char == "(":
                 stack.append(char)
-            else: #char == ")"
-                if stack[-1] == "(":
-                    stack.pop()
-                    if stack and stack[-1] != "(":
-                        stack[-1] += 1
-                    else:
-                        stack.append(1)
-                else: #top of stack is a score
-                    val = stack.pop() * 2
-                    stack.pop()
-                    if stack and stack[-1] != "(":
-                        stack[-1] += val
-                    else:
-                        stack.append(val)
+            else: 
+                val = 1 if stack[-1] == "(" else stack.pop()*2
+                stack.pop()
+                if stack and stack[-1] != "(":
+                    stack[-1] += val
+                else:
+                    stack.append(val)
         return stack[0]
