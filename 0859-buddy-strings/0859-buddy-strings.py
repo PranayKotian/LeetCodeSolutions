4b4b2dec0c2c5f99fc73865f63596ce6c4d3f2cc
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-17 03:18:22 -0500

    Time: 8 ms (5.35%), Space: 16.8 MB (45.67%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -1,23 +1,16 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
         
-        #Counters Solution
+        #Count Swaps Solution
         #Time: O(s+g) Space: O(s+g)
         
-        cs = Counter(s)
-        cg = Counter(goal)
-        
-        if len(s) != len(goal) or cs != cg:
+        if s == goal:
+            return any(i>=2 for i in Counter(s).values())
+        if Counter(s) != Counter(goal):
             return False
         
         swaps = []
         for i in range(len(goal)):
             if goal[i] != s[i]:
                 swaps.append(i)
-        if len(swaps) == 0:
-            return any(i>=2 for i in cs.values())
         return len(swaps) == 2
-        
-        
-        
-            
