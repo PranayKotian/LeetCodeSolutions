96aea5dc7c5e21eb41a7781cfa8ef592c877d2e4
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-17 14:55:11 -0500

    Time: 0 ms (100.00%), Space: 16.8 MB (21.41%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,7 +1,34 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
         
-        #Find First Swap Solution
+        #Check Swaps Solution
+        #Time: O(n) Space: O(1)
+        
+        if len(s1) != len(s2):
+            return False
+        
+        swap = []
+        count = 0
+        
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                if count == 0:
+                    count += 1
+                    swap.append(s1[i])
+                    swap.append(s2[i])
+                elif count == 1:
+                    if s1[i] != swap[1] or s2[i] != swap[0]:
+                        return False
+                    count += 1
+                else:
+                    return False
+        
+        if count == 1:
+            return False
+        return True
+        
+        """
+        #Swap Letters Solution
         #Time: O(n) Space: O(1)
         
         if s1 == s2:
@@ -19,3 +46,4 @@ class Solution:
                     s1[i], s1[first] = s1[first], s1[i]
                     return "".join(s1) == s2
         return False
+        """
