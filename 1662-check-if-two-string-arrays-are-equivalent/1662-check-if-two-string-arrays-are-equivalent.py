44518c0ae26c2e67a48aaeeaccf96b8da48ccafa
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-17 14:30:32 -0500

    Time: 3 ms (13.71%), Space: 16.7 MB (5.06%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,6 +1,19 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         
-        #Python Solution
+        #Generators and Yield Solution
+        #Time: O(n) Space: O(1)
+        
+        def generator(wordList: List[str]):
+            for segment in wordList:
+                for char in segment: 
+                    yield char
+            yield None
+        
+        return all(c1 == c2 for c1,c2 in zip(generator(word1), generator(word2)))
+        
+        """
+        #Naive Python Solution
         #Time: O(n) Space: O(n)
         return "".join(word1) == "".join(word2)
+        """
