d5b7a5a62f8af624e9f394b9617626bf274a6019
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-18 13:30:00 -0500

    Time: 3 ms (34.12%), Space: 16.8 MB (57.55%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        #Brute Force Backtracking Solution
+        #Time: O(2^n) Space: O(n)
+        
+        res = []
+        def generate(cur: str, openParens: int, closeParens: int):
+            if len(cur) == n*2:
+                if openParens == closeParens: 
+                    res.append(cur)
+                return
+            generate(cur+"(", openParens+1, closeParens)
+            if closeParens < openParens:
+                generate(cur+")", openParens, closeParens+1)
+        generate("", 0, 0)
+        return res
