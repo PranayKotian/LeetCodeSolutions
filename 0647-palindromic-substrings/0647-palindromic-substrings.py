afa7af6110f7741430a048a2b7f8a7d022105827
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-18 14:36:59 -0500

    Time: 127 ms (42.47%), Space: 16.5 MB (75.30%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,17 +1,21 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         
-        #Solution 1: Odd and Even length palindromes
-        def countPalis(l,r):
-            res = 0
+        #Find Palindromes Solution
+        #Time: O(n^2) Space: O(1)
+        
+        num_palindromes = 1
+        for i in range(len(s)-1):
+            l = r = i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
+                num_palindromes += 1
                 l -= 1
                 r += 1
-            return res
-        
-        res = 0
-        for i in range(len(s)):
-            res += countPalis(i,i)
-            res += countPalis(i,i+1)
-        return res
+            l = i
+            r = i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                num_palindromes += 1
+                l -= 1
+                r += 1
+        return num_palindromes
+            
