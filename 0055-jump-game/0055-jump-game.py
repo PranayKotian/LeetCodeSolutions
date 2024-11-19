e71ee18cc0d8fca9ca29d7ae4a5ea7564a38042d
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-19 16:23:55 -0500

    Time: 41 ms (32.43%), Space: 17.5 MB (80.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,19 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums)-1
         
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i] >= goal:
-                goal = i
+        '''
+        track max index you can reach
+        iterate through array and update can_reach value as you go
+        if cur index > can_reach: return False
+        '''
         
-        return goal == 0
+        #DP Solution
+        #Time: O(n) Space: O(1)
+        
+        can_reach = 0
+        for i in range(len(nums)):
+            if i <= can_reach:
+                can_reach = max(can_reach, i+nums[i])
+            else:
+                return False
+        return True
