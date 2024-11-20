8fed885f7984d396b62029a81f0e42d006e17cd6
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-20 13:52:42 -0500

    Time: 7 ms (21.92%), Space: 17.3 MB (98.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,6 +4,40 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
+        #Constant Space Solution
+        #Time: O(nm) Space: O(1)
+        
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        
+        first_row = False
+        first_col = False
+        
+        for r in range(ROWS):
+            if matrix[r][0] == 0:
+                first_col = True
+                break
+        for c in range(COLS):
+            if matrix[0][c] == 0:
+                first_row = True
+                
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+        for r in range(1,ROWS):
+            for c in range(1,COLS):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        if first_row:
+            for c in range(COLS):
+                matrix[0][c] = 0
+        if first_col: 
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        
+        """
         #Rows + Columns Solution
         #Time: O(nm) Space: O(n+m)
         
@@ -23,4 +57,4 @@ class Solution:
             for c in range(COLS):
                 if r in zero_rows or c in zero_cols:
                     matrix[r][c] = 0
-        
+        """
