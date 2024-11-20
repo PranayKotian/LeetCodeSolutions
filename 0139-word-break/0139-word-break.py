c7cbbdba01213aabbb6ded0c535ca819bd04e2c8
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-20 14:08:00 -0500

    Time: 2 ms (75.11%), Space: 17.1 MB (8.96%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -4,14 +4,12 @@ class Solution:
         #Recursive Solution w/ Caching
         #Time: O(n^2) Space: O(n) 
         
-        word_set = set(wordDict)
-        
         @cache
         def check_word(word):
-            if word in word_set:
+            if word in wordDict:
                 return True
             for i in range(len(word),0,-1):
-                if word[:i] in word_set:
+                if word[:i] in wordDict:
                     if check_word(word[i:]):
                         return True
             return False
