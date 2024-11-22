618bb4fd7bfe7e77a49eb29d97990532bcb1ee31
Pranay Kotian <lohitkotian@yahoo.ca>
2024-11-21 23:18:50 -0500

    Time: 751 ms (27.80%), Space: 226 MB (10.84%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,39 +1,17 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        #Solution 2: Recursive w/ caching
-        #Time: Space:
-        
-        cache = {}
-        def backtrack(s1,t1):
-            if t1 == "":
+        @cache
+        def distinct_subsequences(s_idx, t_idx):
+            if t_idx == len(t):
                 return 1
-            if s1 == "":
+            if s_idx == len(s):
                 return 0
-            if (s1,t1) in cache:
-                return cache[(s1,t1)]
             
-            if s1[0] != t1[0]:
-                res = backtrack(s1[1:],t1)
-            else:
-                res = backtrack(s1[1:],t1[1:]) + backtrack(s1[1:],t1)
-            cache[(s1,t1)] = res
+            res = 0
+            if s[s_idx] == t[t_idx]:
+                res += distinct_subsequences(s_idx+1, t_idx+1)
+            res += distinct_subsequences(s_idx+1, t_idx)
             return res
-        
-        return backtrack(s,t)
-        
-        """
-        #Solution 1: Recursive solution
-        #Time: O(2^n) Space: O(n)
-        #(Time Limit Exceeded 51/65 passed)
-        
-        if t == "":
-            return 1
-        if s == "":
-            return 0
-        
-        if s[0] != t[0]:
-            return self.numDistinct(s[1:],t)
-        else:
-            return self.numDistinct(s[1:],t[1:]) + self.numDistinct(s[1:],t)
-        """
+            
+        return distinct_subsequences(0,0)
